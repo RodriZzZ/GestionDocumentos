eb4f3418348d59195087c4fa93e7cbce0bf00377
Rodrigo Paz <rodrigopazflores25gmial.com>
2026-03-11 17:33:46 -0600

    Conexion de BD

---
+++ Datos/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GestionDocumentos.Datos
+{
+    public class Conexion
+    {
+        private readonly string cadena = ConfigurationManager.ConnectionStrings["CadenaGestion"].ConnectionString;
+
+        public SqlConnection EstablecerConexion()
+        {
+            return new SqlConnection(cadena);
+        }
+    }
+}
