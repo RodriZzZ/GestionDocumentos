e8ddd14ae0825f4ee54fa09f583615c7466c97d9
fernanortega-hub <fernanortega.sv9292@gmail.com>
2026-03-13 20:24:52 -0600

    feat: add upload file without validations

--- FileDashboard.aspx.cs
+++ FileDashboard.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using GestionDocumentos.Data;
 
 namespace GestionDocumentos
 {
@@ -12,6 +14,28 @@ namespace GestionDocumentos
 
         protected void BtnUploadFile_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var ctx = new GestionDocumentosEntities();
+
+                var filename = Path.GetFileNameWithoutExtension(FupFile.FileName);
+                var extension = Path.GetExtension(FupFile.FileName);
+
+                var id = ctx.sp_UploadNewDocument(
+                    name: filename,
+                    fileExtension: extension,
+                    ownerUserId: 1,
+                    fileContent: FupFile.FileBytes,
+                    fileSizeInBytes: FupFile.PostedFile.ContentLength
+                ).FirstOrDefault();
+
+                LblFileData.Text = id.ToString();
+            }
+            catch (Exception exception)
+            {
+                LblFileData.Text = $"Error: {exception.Message}";
+            }
         }
+
     }
 }
